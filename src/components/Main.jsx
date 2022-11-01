67d7266c9544962e811678b84ab1f774351bf30a
hemjay07 <mujeebopabode07@gmail.com>
2022-11-01 02:25:44 +0100

    rearrange : restructured the links such that the anchor tag is enbeded within the button not the other way round

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,47 +15,49 @@ function Main() {
           className="sharebutton"
           alt=""
         />
-        <img id="profile_img" src={Profile_image} alt="profile image" />
+        <img id="profile__img" src={Profile_image} alt="profile image" />
         <div id="twitter">mujeeb_dimeji</div>
         <div id="slack">mujeeb_dimeji</div>
       </div>
 
       <div className="main--links">
-        <a href="https://twitter.com/mujeeb_dimeji">
-          <button id="btn_twitter">
+        <button id="btn_twitter" className="btn">
+          <a href="https://twitter.com/mujeeb_dimeji">
             <h3>Twitter Link</h3>
-          </button>
-        </a>
-        <a href="https://training.zuri.team/">
-          <button id="btn_zuri">
+          </a>
+        </button>
+
+        <button id="btn__zuri" className="btn">
+          <a href="https://training.zuri.team/">
             <h3>Zuri Team</h3>
-            <p></p>
-          </button>
-        </a>
-        <a href="http://books.zuri.team">
-          <button id="books">
+          </a>
+
+          <p></p>
+        </button>
+        <button id="books" className="btn">
+          <a href="http://books.zuri.team">
             <h3>Zuri Books</h3>
             <p>Books on designs and coding </p>
-          </button>
-        </a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=mujeeb_dimeji">
-          <button id="book_python">
+          </a>
+        </button>
+        <button id="book__python" className="btn">
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=mujeeb_dimeji">
             <h3>Python Books</h3>
             <p>Learn Python the right way</p>
-          </button>
-        </a>
-        <a href="https://background.zuri.team">
-          <button id="pitch">
+          </a>
+        </button>
+        <button id="pitch" className="btn">
+          <a href="https://background.zuri.team">
             <h3>Background Check for Coders</h3>
             <p>Click to do background check on coders</p>
-          </button>
-        </a>
-        <a href="https://books.zuri.team/design-rules">
-          <button id="book_design">
+          </a>
+        </button>
+        <button id="book__design" className="btn">
+          <a href="https://books.zuri.team/design-rules">
             <h3>Design Books</h3>
             <p>Get free design book by Zuri. Yes there is no catch.</p>
-          </button>
-        </a>
+          </a>
+        </button>
       </div>
       <div className="main--footer"></div>
       <img className="slack" src={Slackicon} alt="slack icon" />
